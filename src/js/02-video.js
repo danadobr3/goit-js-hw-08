8bb49b28cf3e77fae6cbe892f89114a599ae83ce
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-10-08 01:55:56 +0200

    hw2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,49 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+const LS_KEY = 'videoplayer-current-time';
+
+let savedTime = localStorage.getItem(LS_KEY) || 0;
+
+const saveCurrentTime = () => {
+    player.getCurrentTime().then((seconds) => {
+        savedTime = seconds;
+        localStorage.setItem(LS_KEY, savedTime);
+    });
+};
+
+const seekToSavedTime = () => {
+    player.setCurrentTime(savedTime).then(() => {
+        // Playback will start from the saved time
+    }).catch((error) => {
+        switch (error.name) {
+            case 'RangeError':
+                // The saved time was less than 0 or greater than the video's duration
+                break;
+
+            default:
+                // Some other error occurred
+                break;
+        }
+    });
+};
+
+player.on('play', () => {
+    saveCurrentTime();
+});
+player.on('timeupdate', throttle(() => {
+    saveCurrentTime();
+}, 1000));
+
+window.addEventListener('beforeunload', () => {
+    saveCurrentTime();
+});
+
+window.addEventListener('load', () => {
+    seekToSavedTime();
+});
