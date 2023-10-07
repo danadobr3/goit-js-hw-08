200d8dbb8eb7b6f11e7b7d59e4537fa94bf2d2a6
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-10-07 23:19:55 +0200

    import 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 const elementsList = document.querySelector('.gallery');
