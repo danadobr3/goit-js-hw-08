90703c687394ad3c4d4afdbae4995fe3bcaef9b4
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-10-07 23:53:31 +0200

    remove import SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
