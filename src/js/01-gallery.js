c9d5857b47094d859b53b603a765c9cc6303983e
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-10-08 12:28:54 +0200

    gallery items js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
